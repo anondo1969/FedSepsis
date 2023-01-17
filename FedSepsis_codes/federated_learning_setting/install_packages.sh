7c73a03a707fa21d7ac0b931e5084efa5f43bbff
Mahbub Ul Alam <mahbub.ul.alam.anondo@gmail.com>
2023-01-17 15:42:11 +0100

    Update install_packages.sh

--- FedSepsis_codes/federated_learning_setting/install_packages.sh
+++ FedSepsis_codes/federated_learning_setting/install_packages.sh
@@ -2,6 +2,11 @@
 
 # run with sudo
 
+#author: Mahbub Ul Alam (mahbub@dsv.su.se)
+#version: 1.0+
+#copyright: Copyright (c) Mahbub Ul Alam (mahbub@dsv.su.se)
+#license : MIT License
+
 apt -y install python3-pip
 pip3 install --upgrade torch==1.8.1
 pip3 install --upgrade wandb
