77fec3936266c9f5f5939f9f86b4f1e9c5864e6f
Mahbub Ul Alam <mahbub.ul.alam.anondo@gmail.com>
2023-01-17 15:42:17 +0100

    Update lstm_trainer.py

--- FedSepsis_codes/federated_learning_setting/lstm_trainer.py
+++ FedSepsis_codes/federated_learning_setting/lstm_trainer.py
@@ -1,3 +1,9 @@
+'''
+@author: Mahbub Ul Alam (mahbub@dsv.su.se)
+@version: 1.0+
+@copyright: Copyright (c) Mahbub Ul Alam (mahbub@dsv.su.se)
+@license : MIT License
+'''
 import logging
 
 import torch
@@ -346,56 +352,7 @@ class LSTMTrainer(ModelTrainer):
         return results_list
 
     def test_on_the_server(self, train_data_local_dict, test_data_local_dict, device, args=None):
-        # logging.info(device)
-        # mse_results_global = []
-        # threshold_dict = {}
-        # thres_func = nn.MSELoss()
-        # #
-        # # opt_threshold = [round(4955.0 * 0.67 / args.batch_size), round(1311.0 * 0.67 / args.batch_size),
-        # #                  round(3910.0 * 0.67 / args.batch_size), round(17524.0 * 0.67 / args.batch_size),
-        # #                  round(6215.0 * 0.67 / args.batch_size), round(9851.0 * 0.67 / args.batch_size),
-        # #                  round(5215.0 * 0.67 / args.batch_size), round(4658.0 * 0.67 / args.batch_size),
-        # #                  round(1953.0 * 0.67 / args.batch_size)]
-        # #
-        # # test_threshold = 1000
-        #
-        # for client_index in train_data_local_dict.keys():
-        #     opt_data = train_data_local_dict[client_index]
-        #     mse_results_per_client = []
-        #     self.model.eval()
-        #     for idx, inp in enumerate(opt_data):
-        #         if idx >= round(len(opt_data) * 2 / 3):
-        #             inp = inp.to(device)
-        #             decode = self.model(inp)
-        #             diff = thres_func(decode, inp)
-        #             mse = diff.item()
-        #             mse_results_per_client.append(mse)
-        #             mse_results_global.append(mse)
-        #     mse_results_per_client = torch.tensor(mse_results_per_client)
-        #     threshold_dict[client_index] = torch.mean(mse_results_per_client) + 1 * torch.std(mse_results_per_client) / np.sqrt(
-        #     args.batch_size)
-        #
-        # # threshold_path = os.path.join("/Users/ultraz/PycharmProjects/FedML-IoT-V/experiments/distributed", 'threshold_dict.pkl')
-        # # joblib.dump(threshold_dict, threshold_path)
-        #
-        #
-        # mse_results_global = torch.tensor(mse_results_global)
-        # threshold_global =torch.mean(mse_results_global) + 1 * torch.std(mse_results_global)/ np.sqrt(args.batch_size)
-        # logging.info('The threshold is %f' % (threshold_global))
-        #
-        # accuracy_array_global = []
-        # precision_array_global = []
-        # fpr_array_global = []
-        #
-        # for client_index in test_data_local_dict.keys():
-        #     test_data = test_data_local_dict[client_index]
-        #     # using global threshold for test
-        #     # [accuracy_client, precision_client, fpr_client] = self.test_local(client_index,
-        #     #                                                                   (test_threshold[client_index] / 2), threshold_global, test_data, device, args)
-        #     [accuracy_client, precision_client, fpr_client] = self.test_local(client_index, threshold_global, train_data_local_dict[client_index], test_data, device, args)
-        #     accuracy_array_global.append(accuracy_client)
-        #     precision_array_global.append(precision_client)
-        #     fpr_array_global.append(fpr_client)
+        
 
         model_save_dir = "../../training"
         path = os.path.join(model_save_dir, 'model.ckpt')
