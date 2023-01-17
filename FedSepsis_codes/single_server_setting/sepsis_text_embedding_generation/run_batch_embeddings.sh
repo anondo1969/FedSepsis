0e07cdee55329bc70ca728a8324350881efabea3
Mahbub Ul Alam <mahbub.ul.alam.anondo@gmail.com>
2023-01-17 15:34:44 +0100

    Update run_batch_embeddings.sh

--- FedSepsis_codes/single_server_setting/sepsis_text_embedding_generation/run_batch_embeddings.sh
+++ FedSepsis_codes/single_server_setting/sepsis_text_embedding_generation/run_batch_embeddings.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#author: Mahbub Ul Alam (mahbub@dsv.su.se)
+#version: 1.0+
+#copyright: Copyright (c) Mahbub Ul Alam (mahbub@dsv.su.se)
+#license : MIT License
+
+
 CUDA_VISIBLE_DEVICES=3 python3 batch_embeddings.py emily &>> emily_log.txt &
 
 CUDA_VISIBLE_DEVICES=3 python3 batch_embeddings.py huang &>> huang_log.txt &
